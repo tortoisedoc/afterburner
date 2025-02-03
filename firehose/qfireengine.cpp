aa757def991b522057afa98340f26ca9a5e4db49
tk <>
2025-02-03 23:21:33 +0200

    fix(qfireengine.cpp): Pass values for channels to the renderer

--- firehose/qfireengine.cpp
+++ firehose/qfireengine.cpp
@@ -15,7 +15,13 @@ private:
     FireEngine *m_fireEngine = nullptr;
     std::vector<unsigned int> m_pixels;
     std::vector<unsigned int> m_pixelsNew;
+    unsigned char m_intensity = 0xFF;
+    unsigned char m_channelRed = 0xFF;
+    unsigned char m_channelGreen = 0;
+    unsigned char m_channelBlue = 0;
+
 protected:
+
     QOpenGLFramebufferObject *createFramebufferObject(const QSize &size) override
     {
         QOpenGLFramebufferObjectFormat format;
@@ -23,30 +29,43 @@ protected:
         //Take care of resizing the engine if the FBO is resized
         if (m_fireEngine)
             delete m_fireEngine;
-        //FBO is managed by Qt
         m_fireEngine = new FireEngine(size.width(), size.height());
+        //FBO is managed by Qt
         m_fbObj = new QOpenGLFramebufferObject(size, format);
         m_pixels.resize(size.width()*size.height());
         m_pixelsNew.resize(size.width()*size.height());
         //ZZap with opacity
         memset(&m_pixels[0], 0xFF000000, size.width()*size.height());
         memset(&m_pixelsNew[0], 0xFF000000, size.width()*size.height());
+        //New fb obj, new texture
         m_iTexIndex = m_fbObj->texture();
         m_size = size;
         return m_fbObj;
     }
 
+
+    void synchronize (QQuickFramebufferObject * _item) override {
+        QFireEngine* item = static_cast<QFireEngine*>(_item);
+        m_intensity = item->getIntensity();
+        m_channelRed = item->getChannelRed();
+        m_channelBlue = item->getChannelBlue();
+        m_channelGreen = item->getChannelGreen();
+    }
+
+
     void render() override
     {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         m_fireEngine->Scroll(&m_pixels[0], 1);
-        m_fireEngine->Ignite(&m_pixels[0], m_size.height() - 1);
+        m_fireEngine->Ignite(&m_pixels[0], m_size.height() - 1, m_channelRed, m_channelGreen, m_channelBlue, m_intensity);
         m_fireEngine->Burn(&m_pixels[0], &m_pixelsNew[0], 1, 1);
         //This is a bit simple, and could be optimized. But for now will do!
         m_pixels = m_pixelsNew;
 
         //Very simple old-school OpenGL way of blasting out a texture. First thing that comes to mind, really, and straightforward.
         glBindTexture (GL_TEXTURE_2D, m_fbObj->texture());
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_BLEND);
         glEnable(GL_TEXTURE_2D);
         //Lets use the simple texture we get from the fb object kthxbye
         glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, m_size.width(), m_size.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, &m_pixels[0]);
@@ -63,11 +82,19 @@ protected:
         //Again please
         update();
     }
+
     ~QFireEngineRenderer (){
         delete m_fireEngine;
         //Fbo taken care of by qt
     }
 };
+
+QFireEngine::QFireEngine() {
+    m_intensity = 0xFF;
+    m_channelRed = 0xFF;
+    m_channelGreen = 0xFF;
+    m_channelBlue = 0xFF;
+}
 
 QQuickFramebufferObject::Renderer *QFireEngine::createRenderer() const
 {
--- firehose/qfireengine.h
+++ firehose/qfireengine.h
@@ -6,6 +6,65 @@
 class QFireEngine : public QQuickFramebufferObject
 {
     Q_OBJECT
-private:    
+    QML_ELEMENT
+    Q_PROPERTY(unsigned char intensity READ getIntensity WRITE setIntensity NOTIFY intensityChanged)
+    Q_PROPERTY(unsigned char redChannel READ getChannelRed WRITE setChannelRed NOTIFY redChannelChanged)
+    Q_PROPERTY(unsigned char greenChannel READ getChannelGreen WRITE setChannelGreen NOTIFY greenChannelChanged)
+    Q_PROPERTY(unsigned char blueChannel READ getChannelBlue WRITE setChannelBlue NOTIFY blueChannelChanged)
+
+public:
+    QFireEngine();
+
+    unsigned char getIntensity() {
+        return m_intensity;
+    }
+
+    void setIntensity(unsigned char value) {
+        m_intensity = value;
+        emit intensityChanged();
+        update();
+    }
+
+    unsigned char getChannelRed() {
+        return m_channelRed;
+    }
+
+    void setChannelRed(unsigned char value) {
+        m_channelRed = value;
+        emit redChannelChanged();
+        update();
+    }
+
+    unsigned char getChannelGreen() {
+        return m_channelGreen;
+    }
+
+    void setChannelGreen(unsigned char value) {
+        m_channelGreen = value;
+        emit greenChannelChanged();
+        update();
+    }
+
+    unsigned char getChannelBlue() {
+        return m_channelBlue;
+    }
+
+    void setChannelBlue(unsigned char value) {
+        m_channelBlue = value;
+        emit blueChannelChanged();
+        update();
+    }
+
+signals:
+    void intensityChanged();
+    void redChannelChanged();
+    void greenChannelChanged();
+    void blueChannelChanged();
+protected:
     QQuickFramebufferObject::Renderer *createRenderer() const override;
+private:
+    unsigned char m_intensity;
+    unsigned char m_channelRed;
+    unsigned char m_channelGreen;
+    unsigned char m_channelBlue;
 };

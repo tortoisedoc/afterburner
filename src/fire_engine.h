bfc79baa055ab69f13bd6ccd4477a661532717c5
tk <>
2025-02-06 00:20:34 +0200

    fix(fire_engine.h): Moved LOG here, added post processing

--- src/fire_engine.h
+++ src/fire_engine.h
@@ -1,3 +1,13 @@
+#pragma once
+
+//Add logging flag to cmake for logging
+#ifdef LOGGING
+#define LOG(cmd)\
+    std::cout << cmd
+#else
+#define LOG(cmd)
+#endif
+
 class FireEngine {
 private:
   int m_width;
@@ -12,11 +22,16 @@ public:
   /**
     feeds the fire with new combustion at the bottom
   */
-  void Ignite(unsigned int *pixels, int verticalOffset, unsigned char chRed, unsigned char chGreen, unsigned char chBlue, unsigned char intensity);
+  void Ignite(unsigned int *pixels, int verticalOffset,
+      unsigned char chRed, unsigned char chGreen, unsigned char chBlue, unsigned char intensity);
   /**
-     makes the fire "burn"
+    makes the fire "burn"
   */
   void Burn(unsigned int *pixels, unsigned int *pixelsOut, int nrPasses, int fading);
+  /**
+    postprocess the fire
+  */
+  void PostProcess(unsigned int *pixels, unsigned int *pixel);
   /**
     grows the flames
   */
